71231ca097a58f4a944e0c0d3255be2f21c31ff5
lavikara <karatemitayo@gmail.com>
2023-05-05 04:53:05 +0100

    made template responsive

--- src/utils/templates.js
+++ src/utils/templates.js
@@ -1,12 +1,12 @@
 const templates = {
   textTemplate: `<div id="textElement" draggable="true" class="tw-absolute tw-top-[20px] tw-left-[20px] tw-cursor-move">
-                  <div id="headerText" class="tw-relative tw-h-[40px]">
-                    <p contentEditable="false" class="tw-h-[40px] tw-text-white tw-text-lg xs:tw-text-2xl tw-font-bold tw-text-center">
+                  <div id="headerText" class="tw-relative tw-h-full tw-min-w-[200px]">
+                    <p contentEditable="false" class="tw-h-full tw-text-white tw-text-lg xs:tw-text-2xl tw-font-bold tw-text-center">
                       Edit text here
                     </p>
                   </div>
                 </div>`,
-  imageTemplate: `<div draggable="true" class="tw-absolute tw-top-[60px] tw-left-20px tw-h-[55px] tw-w-[200px] tw-cursor-move">
+  imageTemplate: `<div draggable="true" class="tw-absolute tw-top-[60px] tw-left-20px tw-h-full tw-w-full tw-cursor-move">
                     <img id="image"/>                 
                   </div>`,
   buttonTemplate: `<div id="buttonElement" draggable="true" class="tw-absolute tw-top-[60px] tw-left-20px tw-h-[55px] tw-w-[200px] tw-cursor-move">                      
